03b0b3d0e717af3800ed0f52784c1834d3e9bb54
norplys <joywinata88@gmail.com>
2024-11-26 11:21:43 +0700

    fix: fixing env and pre-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 /* eslint-disable no-console */
 import express from 'express';
 import { createServer } from 'http';
-import { loadEnv } from './utils/env.js';
+import { appEnv } from './utils/env.js';
 import loaders from './loaders/index.js';
 import routes from './routes/index.js';
 import errorHandler from './middlewares/error.js';
 
 function main() {
-  loadEnv();
-
   const app = express();
   const server = createServer(app);
-  const port = process.env.PORT || process.env.HOST_PORT;
+  const port = appEnv.PORT;
 
   loaders(app, server);
 
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-console */
 import dotenv from 'dotenv';
+import { access } from 'fs/promises';
 
-export async function loadEnv() {
+async function loadEnv() {
   const isRunningInDevelopment = process.env.NODE_ENV === 'development';
 
   let envPath = undefined;
 
   if (isRunningInDevelopment) {
+    const isLocalEnvExists = await access('.env.local')
+      .then(() => true)
+      .catch(() => false);
+
+    if (!isLocalEnvExists) {
+      throw new Error('Local environment file (.env.local) is missing');
+    }
+
     envPath = '.env.local';
 
     console.log(`Loading environment variables from ${envPath}`);
@@ -16,3 +25,16 @@ export async function loadEnv() {
 
   dotenv.config({ path: envPath });
 }
+
+function formatEnv() {
+  const PORT = process.env.PORT ?? process.env.HOST_PORT;
+
+  return {
+    ...process.env,
+    PORT
+  };
+}
+
+await loadEnv();
+
+export const appEnv = formatEnv();
