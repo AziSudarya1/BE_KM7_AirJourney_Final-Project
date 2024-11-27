82aab1ba1385417cedeba75732a0fb33279cb3f4
adibnajwan <adibnajwan@gmail.com>
2024-11-28 02:15:08 +0700

    modified validation for reset password

--- src/middlewares/validation/auth.js
+++ src/middlewares/validation/auth.js
@@ -33,3 +33,21 @@ export async function resetPasswordRequestValidation(req, res, next) {
     return res.status(400).json({ message: errorMessages });
   }
 }
+
+const resetPasswordValidationSchema = Joi.object({
+  token: Joi.string().required(),
+  newPassword: Joi.string.min(6).required
+});
+
+export async function resetPasswordValidation(req, res, next) {
+  try {
+    await resetPasswordValidationSchema.validateAsync(req.body, {
+      abortEarly: false
+    });
+
+    next();
+  } catch (error) {
+    const errorMessages = generateJoiError(error);
+    return res.status(400).json({ message: errorMessages });
+  }
+}
