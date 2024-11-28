d7394a5d9b24d97790714d593fa44f155d7067af
adibnajwan <adibnajwan@gmail.com>
2024-11-28 13:38:23 +0700

    add logic for send reset password email

--- src/utils/email/mail.js
+++ src/utils/email/mail.js
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer';
 import { appEnv } from '../env.js';
+const ejs = require('ejs');
+const path = require('path');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -15,5 +17,18 @@ export async function sendEmail(to, subject, html) {
     to,
     subject,
     html
+  });
+}
+
+export async function resetPasswordEmail(to, subject, variables) {
+  const templatePath = path.join(process.cwd(), 'template/reset-password.ejs');
+
+  const htmlContent = await ejs.renderFile(templatePath, variables);
+
+  const info = await transporter.sendMail({
+    from: appEnv.EMAIL_ADDRESS,
+    to,
+    subject,
+    html: htmlContent
   });
 }
