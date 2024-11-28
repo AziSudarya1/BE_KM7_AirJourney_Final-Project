1f51949448ce75d00b56c87c7f0e0efe308b75cc
adibnajwan <adibnajwan@gmail.com>
2024-11-28 21:39:13 +0700

    move all controllers for reset password to passwordReset.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -10,33 +10,3 @@ export async function login(req, res) {
     data: result
   });
 }
-
-export async function sendResetPasswordEmail(req, res) {
-  const { email } = req.body;
-
-  await authService.sendResetPasswordEmail(email);
-
-  res.status(200).json({
-    message: 'Reset password email send successfully'
-  });
-}
-
-export async function validateResetPasswordToken(req, res) {
-  const { token } = req.params;
-
-  await authService.validateResetPasswordToken(token);
-
-  res.status(200).json({
-    message: 'Reset password token is valid'
-  });
-}
-
-export async function resetPassword(req, res) {
-  const { token, newPassword } = req.body;
-
-  await authService.resetPassword(token, newPassword);
-
-  res.status(200).json({
-    message: 'Password reset successfully'
-  });
-}
