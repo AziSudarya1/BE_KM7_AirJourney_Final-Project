489c9d65953a64cd5f50674b7c70b6e184bc4ef1
norplys <joywinata88@gmail.com>
2024-11-28 19:22:28 +0700

    fix: fixing dynamic cors

--- src/loaders/cors.js
+++ src/loaders/cors.js
@@ -1,8 +1,20 @@
 import cors from 'cors';
 import { appEnv } from '../utils/env.js';
+import { HttpError } from '../utils/error.js';
+
+const ALLOWED_ORIGINS = appEnv.VALID_ORIGINS.split(',');
+
+if (!ALLOWED_ORIGINS.length) {
+  throw new Error('There are no allowed origins');
+}
 
 export const corsOptions = {
-  origin: appEnv.FRONTEND_URL,
+  origin: (origin, callback) => {
+    const isOriginAllowed = origin && ALLOWED_ORIGINS.includes(origin);
+
+    if (isOriginAllowed) callback(null, true);
+    else callback(new HttpError(403, { message: 'Forbidden by CORS' }));
+  },
   credentials: true
 };
 
