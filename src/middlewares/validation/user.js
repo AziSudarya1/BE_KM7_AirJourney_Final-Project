cbc67f95c8c85edd5745ca5d2af353d3fe8b2813
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-12-02 10:37:52 +0700

    feat:update user

--- src/middlewares/validation/user.js
+++ src/middlewares/validation/user.js
@@ -8,13 +8,36 @@ const createUserSchema = Joi.object({
   phoneNumber: Joi.string()
     .pattern(/^\+62[0-9]+$/)
     .min(11)
-    .max(13)
+    .required()
+});
+
+const updateUserSchema = Joi.object({
+  name: Joi.string().min(3).max(50),
+  email: Joi.string().email(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+62[0-9]+$/)
+    .min(11)
     .required()
 });
 
 export async function createUserValidation(req, res, next) {
   try {
     await createUserSchema.validateAsync(req.body, { abortEarly: false });
+
+    next();
+  } catch (error) {
+    if (Joi.isError(error)) {
+      const errorMessages = generateJoiError(error);
+      return res.status(400).json({ message: errorMessages });
+    }
+
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+export async function updateUserValidation(req, res, next) {
+  try {
+    await updateUserSchema.validateAsync(req.body, { abortEarly: false });
 
     next();
   } catch (error) {
