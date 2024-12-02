fd1f37864297a3f406b412660b26c2c99e40cc17
adibnajwan <adibnajwan@gmail.com>
2024-12-02 22:10:26 +0700

    fix: controllers for create and update airline

--- src/controllers/airline.js
+++ src/controllers/airline.js
@@ -1,11 +1,32 @@
 import * as airlineServices from '../services/airline.js';
 
 export async function createAirline(req, res) {
-  const airlineData = await airlineServices.createAirline(req.body);
+  const { code, name, image } = req.body;
+  const airlineData = await airlineServices.createAirline({
+    code,
+    name,
+    image
+  });
 
   res.status(201).json({
     message: 'Airline created successfully',
     airlineData
+  });
+}
+
+export async function updateAirline(req, res) {
+  const { id } = req.params;
+  const { code, name, image } = req.body;
+
+  const airlineData = await airlineServices.updateAirlineById(id, {
+    code,
+    name,
+    image
+  });
+
+  res.status(200).json({
+    message: 'Airline updated successfully',
+    data: airlineData
   });
 }
 
@@ -16,17 +37,6 @@ export async function getAirlineById(req, res) {
 
   res.status(200).json({
     message: 'Airline retrieved by ID successfully',
-    data: airlineData
-  });
-}
-
-export async function updateAirline(req, res) {
-  const { id } = req.params;
-
-  const airlineData = await airlineServices.updateAirlineById(id, req.body);
-
-  res.status(200).json({
-    message: 'Airline updated successfully',
     data: airlineData
   });
 }
