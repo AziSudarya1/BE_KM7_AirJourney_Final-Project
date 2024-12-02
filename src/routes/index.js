39284e1a9e521e8c30fc04728a0c1bdd35de8a24
adibnajwan <adibnajwan@gmail.com>
2024-12-02 18:39:33 +0700

    feat: routes in index.js for airline

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import root from './root.js';
 import auth from './auth.js';
 import user from './user.js';
 import docs from './docs.js';
+import airline from './airline.js';
 
 export default (app) => {
   const router = Router();
@@ -13,4 +14,5 @@ export default (app) => {
   user(router);
   root(router);
   docs(router);
+  airline(router);
 };
