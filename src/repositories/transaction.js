cc4b94279d549cc1ea54f167aeb86405ec4b891a
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-12-07 21:52:39 +0700

    fix: existingTransaction and delete field statuf from table trasaction

--- src/repositories/transaction.js
+++ src/repositories/transaction.js
@@ -4,13 +4,17 @@ export function createTransactionAndPassenger(payload) {
   return prisma.transaction.create({
     data: {
       amount: payload.amount,
-      status: 'UNPAID',
       userId: payload.userId,
       departureFlightId: payload.departureFlightId,
       returnFlightId: payload.returnFlightId || null,
       Passenger: {
         createMany: {
           data: payload.passengers
+        }
+      },
+      payment: {
+        create: {
+          status: 'PENDING'
         }
       }
     }
@@ -21,7 +25,9 @@ export function getBookingTransaction(id) {
   return prisma.transaction.findFirst({
     where: {
       userId: id,
-      status: 'UNPAID'
+      payment: {
+        status: 'PENDING'
+      }
     }
   });
 }
