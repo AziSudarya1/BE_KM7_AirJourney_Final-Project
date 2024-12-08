98a26b3a48f86bae7cdd0f27daec290db8ddf05b
AziSudarya1 <azisudarya98@gmail.com>
2024-12-08 19:22:47 +0700

    fix: fixing notification

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,11 +1,24 @@
 import * as notificationRepository from '../repositories/notification.js';
+import { HttpError } from '../utils/error.js';
+import * as userRepository from '../repositories/user.js';
 
 export async function createNotification(payload) {
+  const user = await userRepository.getUserWithId(payload.userId);
+
+  if (!user) {
+    throw new HttpError('User not found!', 404);
+  }
+
   const data = await notificationRepository.createNotification(payload);
 
   return data;
 }
 
+export async function getNotificationId(id) {
+  const data = await notificationRepository.checkNotificationId(id);
+
+  return data;
+}
 export async function checkNotificationId(id) {
   const data = await notificationRepository.checkNotificationId(id);
 
@@ -14,6 +27,12 @@ export async function checkNotificationId(id) {
 
 export async function checkUserId(userId) {
   const data = await notificationRepository.checkUserId(userId);
+
+  return data;
+}
+
+export async function getNotification(userId) {
+  const data = await notificationRepository.getNotification(userId);
 
   return data;
 }
@@ -30,8 +49,8 @@ export async function updateNotification(id, userId) {
   return data;
 }
 
-export async function updateAllNotification(id, userId) {
-  const data = await notificationRepository.updateAllNotification(id, userId);
+export async function updateAllNotification(userId) {
+  const data = await notificationRepository.updateAllNotification(userId);
 
   return data;
 }
