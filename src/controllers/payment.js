5dca103b46dd29f6e953d68ad199c1b7e1cbca9c
adibnajwan <adibnajwan@gmail.com>
2024-12-10 14:01:29 +0700

    feat: controller for payment

---
+++ src/controllers/payment.js
@@ -0,0 +1,22 @@
+import * as paymentService from '../services/payment.js';
+
+export async function initiatePayment(req, res) {
+  const { transactionId, amount } = req.body;
+
+  const paymentUrl = await paymentService.createPayment(transactionId, amount);
+  res.status(201).json({
+    message: 'Payment initiated successfully',
+    paymentUrl
+  });
+}
+
+export async function handleWebhook(req, res) {
+  try {
+    const { transaction_status, order_id } = req.body;
+
+    await paymentService.updateTransactionStatus(order_id, transaction_status);
+    res.status(200).json({ message: 'Transaction status updated' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
