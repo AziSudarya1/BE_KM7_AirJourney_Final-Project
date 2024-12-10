94787ec420a47e6ead61ffad217713ae7c71cb6b
adibnajwan <adibnajwan@gmail.com>
2024-12-10 14:02:05 +0700

    feat: rotes for payment

---
+++ src/routes/payment.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import * as paymentController from '../controllers/payment.js';
+import * as paymentValidation from '../middlewares/validation/payment.js';
+import { verifyMidtransSignature } from '../middlewares/webhookVerifier.js';
+import * as authMiddleware from '../middlewares/auth.js';
+
+export default (app) => {
+  const router = Router();
+
+  app.use('/payment', router);
+
+  router.post(
+    '/initiate',
+    authMiddleware.isAuthorized,
+    paymentValidation.validatePaymentRequest,
+    paymentController.initiatePayment
+  );
+
+  router.post(
+    '/webhook',
+    verifyMidtransSignature,
+    paymentController.handleWebhook
+  );
+};
