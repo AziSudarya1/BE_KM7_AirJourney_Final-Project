6fa9db894478ebc233e6b8a848f0d1f6de70a6a3
adibnajwan <adibnajwan@gmail.com>
2024-12-12 20:24:47 +0700

    feat: routes payment in index

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,7 @@ import airlines from './airlines.js';
 import notifications from './notifications.js';
 import transactions from './transactions.js';
 import uploads from './uploads.js';
+import payment from './payment.js';
 
 export default (app) => {
   const router = Router();
@@ -27,4 +28,5 @@ export default (app) => {
   notifications(router);
   transactions(router);
   uploads(router);
+  payment(router);
 };
