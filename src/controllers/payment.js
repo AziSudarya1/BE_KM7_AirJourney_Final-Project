d81623b7e1befcdf50612ff174229a49c2cb2b17
adibnajwan <adibnajwan@gmail.com>
2024-12-13 21:00:16 +0700

    drop: initiate payment feature

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -1,15 +1,5 @@
 import * as paymentService from '../services/payment.js';
 
-export async function initiatePayment(req, res) {
-  const { transactionId } = req.body;
-
-  const paymentUrl = await paymentService.createMidtransToken(transactionId);
-  res.status(201).json({
-    message: 'Payment initiated successfully',
-    paymentUrl
-  });
-}
-
 export async function handleWebhook(req, res) {
   try {
     const { transaction_status, order_id } = req.body;
--- src/routes/payments.js
+++ src/routes/payments.js
@@ -1,21 +1,12 @@
 import { Router } from 'express';
 import * as paymentController from '../controllers/payment.js';
-import * as paymentValidationMiddleware from '../middlewares/validation/payment.js';
 import * as midtransMiddleware from '../middlewares/midtrans.js';
-import * as authMiddleware from '../middlewares/auth.js';
 import * as authKeyMiddleware from '../middlewares/authKey.js';
 
 export default (app) => {
   const router = Router();
 
   app.use('/payments', router);
-
-  router.post(
-    '/initiate',
-    authMiddleware.isAuthorized,
-    paymentValidationMiddleware.validatePaymentRequest,
-    paymentController.initiatePayment
-  );
 
   router.post(
     '/webhook',
