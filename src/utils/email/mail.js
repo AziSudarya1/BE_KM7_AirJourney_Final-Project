36590cb55da8afc753cf574cb38364e8b2799845
norplys <joywinata88@gmail.com>
2024-12-15 12:41:18 +0700

    feat: ad mjml and handleBard

--- src/utils/email/mail.js
+++ src/utils/email/mail.js
@@ -1,5 +1,14 @@
 import nodemailer from 'nodemailer';
+import fs from 'fs';
+import Handlebars from 'handlebars';
 import { appEnv } from '../env.js';
+
+function _compileTemplate(fileName, data) {
+  const templateFile = fs.readFileSync(`./build/${fileName}.html`, 'utf8');
+  const compiled = Handlebars.compile(templateFile);
+
+  return compiled(data);
+}
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
