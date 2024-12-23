3f9232e2801c47b0c2d70698df853f0424408ba5
norplys <joywinata88@gmail.com>
2024-12-23 11:59:32 +0700

    fix: clearing migration file

--- src/utils/prisma/migrations/20241207051119_change_null_departure_seat/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "passenger" DROP CONSTRAINT "passenger_departure_seat_id_fkey";
-
--- AlterTable
-ALTER TABLE "passenger" ALTER COLUMN "departure_seat_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "passenger" ADD CONSTRAINT "passenger_departure_seat_id_fkey" FOREIGN KEY ("departure_seat_id") REFERENCES "seat"("id") ON DELETE SET NULL ON UPDATE CASCADE;
